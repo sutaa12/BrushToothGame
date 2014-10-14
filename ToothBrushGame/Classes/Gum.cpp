5184e5d6bbe621ad0345607e1a206996c3e3c69d
JunMaru <toruneron@gmail.com>
2014-10-14 13:25:35 +0900

    歯茎クラスの追加
    歯茎クラスを新規追加

---
+++ ToothBrushGame/Classes/Gum.cpp
@@ -0,0 +1,9 @@
+//
+//  Gum.cpp
+//  ToothBrushGame
+//
+//  Created by 丸山 潤 on 2014/10/14.
+//
+//
+
+#include "Gum.h"
---
+++ ToothBrushGame/Classes/Gum.h
@@ -0,0 +1,14 @@
+//
+//  Gum.h
+//  ToothBrushGame
+//
+//  Created by 丸山 潤 on 2014/10/14.
+//
+//
+
+#ifndef __ToothBrushGame__Gum__
+#define __ToothBrushGame__Gum__
+
+#include <stdio.h>
+
+#endif /* defined(__ToothBrushGame__Gum__) */
