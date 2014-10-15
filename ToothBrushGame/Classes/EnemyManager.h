6e69671d82a699a245ae30762eab7984c16fb232
RazeLiberty <battledroid@outlook.jp>
2014-10-15 11:48:31 +0900

    敵マネージャー追加しといたぜ。
    あとは任せたぜ。

---
+++ ToothBrushGame/Classes/EnemyManager.h
@@ -0,0 +1,64 @@
+//
+//  EnemyManager.h
+//  ToothBrushGame
+//
+//  Created by 川原 岳大 on 2014/10/15.
+//
+//
+
+#ifndef __ToothBrushGame__EnemyManager__
+#define __ToothBrushGame__EnemyManager__
+
+#include <stdio.h>
+
+//********************************************************************************
+// インクルード
+//********************************************************************************
+#include "cocos2d.h"
+
+//********************************************************************************
+// 名前空間の使用
+//********************************************************************************
+using namespace cocos2d;
+
+//********************************************************************************
+// 前方クラス宣言
+//********************************************************************************
+class Enemy;
+
+//********************************************************************************
+// クラス宣言
+//********************************************************************************
+class EnemyManager
+{
+public:
+    EnemyManager();
+    ~EnemyManager();
+
+    bool init(void);
+    void uninit(void);
+    void update(void);
+
+    static EnemyManager* Create(const Vec2& startLeftTopPos,Layer* layer);
+
+    Enemy* getTopTooth(void){return m_pTopTooth;}
+    Enemy* getBootomTooth(void){return m_pBottomTooth;}
+
+    void disappear(const Vec2& vec);
+    void action(const Vec2& vec);
+    void attack(const Vec2& vec);
+    void move(const Vec2& vec);
+    void spawn(const Vec2& vec);
+    void checkDamage(const Vec2& vec);
+
+
+private:
+    Vec2 m_startLeftTopPos;
+
+    Enemy* m_pTopTooth;
+    Enemy* m_pBottomTooth;
+
+    Layer* m_pLayer;
+};
+
+#endif /* defined(__ToothBrushGame__EnemyManager__) */
