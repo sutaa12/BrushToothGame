5253a90d2c0cb4b9b70abb3beedee9afe12e57b3
sutaa12 <snaritada@gmail.com>
2014-10-15 12:36:27 +0900

    数字とスコア作成 数字、スコア作成

---
+++ ToothBrushGame/Classes/Score.cpp
@@ -0,0 +1,95 @@
+//
+//  Score.cpp
+//  ToothBrushGame
+//
+//  Created by 鈴木 愛忠 on 2014/10/15.
+//
+//
+
+//********************************************************************************
+// インクルード
+#include "Score.h"
+#include "Number.h"
+
+static const int Score_DISPLAY_CENTER_X = (320);
+
+//================================================================================
+// コンストラクタ
+//================================================================================
+Score::Score(void)
+{
+    // メンバ変数の初期化
+    m_ppNumbers = nullptr;
+    m_pLayer = nullptr;
+    m_nMaxNumber = 0;
+}
+
+//================================================================================
+// デストラクタ
+//================================================================================
+Score::~Score()
+{
+    
+}
+
+//================================================================================
+// 初期化処理
+//================================================================================
+bool Score::init(void)
+{
+    m_ppNumbers = new Number*[m_nMaxNumber];
+    for(int nloop = 0;nloop < m_nMaxNumber;nloop++)
+    {
+        // 上歯茎生成
+        m_ppNumbers[nloop] = Number::Create();
+        
+        // スプライトサイズ取得
+        Rect numgerRect = (m_ppNumbers[nloop]->getSprite()->getBoundingBox());
+        
+        // 座標変換(左上を持ってきているため、中心にそろえる処理)
+        m_ppNumbers[nloop]->setPos(Vec2(Score_DISPLAY_CENTER_X,
+                               m_startLeftTopPos.y - (numgerRect.size.height / 2)));
+        
+        // スプライトの再配置
+        m_ppNumbers[nloop]->RefreshSpritePos();
+        
+        // スプライトの登録
+        m_pLayer->addChild(m_ppNumbers[nloop]->getSprite());
+    }
+    // 正常終了
+    return true;
+}
+
+//================================================================================
+// 終了処理
+//================================================================================
+void Score::uninit(void)
+{
+    
+}
+
+//================================================================================
+// 更新処理
+//================================================================================
+void Score::update(void)
+{
+    
+}
+
+//================================================================================
+// 生成処理
+//================================================================================
+Score* Score::Create(const Vec2& startLeftTopPos,int nMaxNumber,Layer* layer)
+{
+    // 歯マネージャーのインスタンス化
+    Score* pScore = new Score();
+    
+    // メンバー変数の代入
+    pScore->m_startLeftTopPos = startLeftTopPos;
+    pScore->m_pLayer = layer;
+    pScore->m_nMaxNumber = nMaxNumber;
+    // 初期化
+    pScore->init();
+    
+    return pScore;
+}
---
+++ ToothBrushGame/Classes/Score.h
@@ -0,0 +1,54 @@
+//
+//  Score.h
+//  ToothBrushGame
+//
+//  Created by 鈴木 愛忠 on 2014/10/15.
+//
+//
+
+#ifndef __ToothBrushGame__Score__
+#define __ToothBrushGame__Score__
+
+//********************************************************************************
+// インクルード
+//********************************************************************************
+#include "cocos2d.h"
+
+//********************************************************************************
+// 名前空間の使用
+//********************************************************************************
+using namespace cocos2d;
+
+//********************************************************************************
+// 前方クラス宣言
+//********************************************************************************
+class Number;
+
+//********************************************************************************
+// クラス宣言
+//********************************************************************************
+class Score
+{
+public:
+    Score();
+    ~Score();
+    
+    bool init(void);
+    void uninit(void);
+    void update(void);
+    
+    static Score* Create(const Vec2& startLeftTopPos,int nNumber,Layer* layer);
+    
+    Number** getNumber(void){return &m_ppNumbers[0];}
+        
+private:
+    Vec2 m_startLeftTopPos;
+    
+    Number** m_ppNumbers;
+    
+    Layer* m_pLayer;
+    
+    int m_nMaxNumber;//数字の桁数
+};
+
+#endif /* defined(__ToothBrushGame__Score__) */
