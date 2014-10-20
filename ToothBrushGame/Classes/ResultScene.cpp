f45240652c039a71c418d68f1bc68786d7ebfbf0
JunMaru <toruneron@gmail.com>
2014-10-20 10:15:41 +0900

    シーン遷移時、タッチができなくなる現象の修正
    新しいシーンをクリエイトした後イベントを消していたが、順番を逆にして
    イベントを消した後新しいシーンを生成に

--- ToothBrushGame/Classes/ResultScene.cpp
+++ ToothBrushGame/Classes/ResultScene.cpp
@@ -107,11 +107,12 @@ bool ResultScene::onTouchBegin(Touch* pTouch,Event* pEvent)
 {
     // タッチ座標の取得
     m_touchPos = pTouch->getLocation();
-    
-    Director::getInstance()->replaceScene(TransitionFade::create(1.0f,TitleScene::createScene(),Color3B::WHITE));
+
     this->getEventDispatcher()->removeAllEventListeners();
     this->removeAllChildren();
-    
+
+    Director::getInstance()->replaceScene(TransitionFade::create(1.0f,TitleScene::createScene(),Color3B::WHITE));
+
     return true;
 }
 
