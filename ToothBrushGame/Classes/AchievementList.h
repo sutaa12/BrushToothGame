0f11b0278c94d6804a7c175494e4fd9010cc96b6
sutaa12 <snaritada@gmail.com>
2014-10-23 16:48:41 +0900

    実績リストのからファイル作成
    まだ何もしてない

---
+++ ToothBrushGame/Classes/AchievementList.h
@@ -0,0 +1,14 @@
+//
+//  AchievementList.h
+//  ToothBrushGame
+//
+//  Created by 鈴木 愛忠 on 2014/10/23.
+//
+//
+
+#ifndef __ToothBrushGame__AchievementList__
+#define __ToothBrushGame__AchievementList__
+
+#include <stdio.h>
+
+#endif /* defined(__ToothBrushGame__AchievementList__) */
---
+++ ToothBrushGame/Classes/AchievementListManager.h
@@ -0,0 +1,14 @@
+//
+//  AchievementListManager.h
+//  ToothBrushGame
+//
+//  Created by 鈴木 愛忠 on 2014/10/23.
+//
+//
+
+#ifndef __ToothBrushGame__AchievementListManager__
+#define __ToothBrushGame__AchievementListManager__
+
+#include <stdio.h>
+
+#endif /* defined(__ToothBrushGame__AchievementListManager__) */
