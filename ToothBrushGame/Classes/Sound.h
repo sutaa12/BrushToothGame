90d66f2c414055d91d8311a9bdd0a06d79f896bd
RazeLiberty <battledroid@outlook.jp>
2014-10-29 14:58:29 +0900

    使ってないリソースの削除

--- ToothBrushGame/Classes/Sound.h
+++ ToothBrushGame/Classes/Sound.h
@@ -37,6 +37,8 @@ using namespace CocosDenshion;
 #define SE_VOLUME_3 (0.3f)
 #define SE_VOLUME_HALF (0.5f)
 #define SE_VOLUME_MAX (1.0f)
+
+//敵昇天音量
 #define SE_ENEMY_DOWN_VOLUME_3 (0.3f)
 #define SE_ENEMY_DOWN_VOLUME_5 (0.5f)
 
@@ -48,15 +50,6 @@ using namespace CocosDenshion;
 
 #define BGM_TITLE_1 ("bgm_title_1.mp3")
 #define BGM_TITLE_2 ("bgm_title_2.mp3")
-
-#define BGM_BOSS_SCENE_1 ("bgm_boss_scene_1.mp3")
-#define BGM_BOSS_SCENE_2 ("bgm_boss_scene_2.mp3")
-#define BGM_BOSS_SCENE_3 ("bgm_boss_scene_3.mp3")
-#define BGM_BOSS_SCENE_4 ("bgm_boss_scene_4.mp3")
-#define BGM_BOSS_SCENE_5 ("bgm_boss_scene_5.mp3")
-#define BGM_BOSS_SCENE_6 ("bgm_boss_scene_6.mp3")
-#define BGM_BOSS_SCENE_7 ("bgm_boss_scene_7.mp3")
-#define BGM_BOSS_SCENE_8 ("bgm_boss_scene_8.mp3")
 
 #define BGM_ENEMY_SCENE_1 ("bgm_enemy_scene_1.mp3")
 #define BGM_ENEMY_SCENE_2 ("bgm_enemy_scene_2.mp3")
@@ -78,51 +71,31 @@ using namespace CocosDenshion;
 
 //ボタン
 #define SE_BUTTON_1 ("se_button_1.mp3")
-#define SE_BUTTON_2 ("")
-
 #define SE_START_BUTTON_1 ("se_game_start_1.mp3")
 
-
-/*
-//歯垢
-#define SE_PLAQUE_DOWN_1 ("se_plaque_down_1.mp3")
-#define SE_PLAQUE_DOWN_2 ("se_plaque_down_2.mp3")
-#define SE_PLAQUE_DOWN_3 ("se_plaque_down_3.mp3")
-*/
+//実績解除音
+#define SE_GET_ACHIEVEMENT_3 ("get_achievement_3.mp3")
+#define SE_GET_ACHIEVEMENT_4 ("get_achievement_4.mp3")
 
 //敵
 #define SE_ENEMY_DOWN_1 ("hit_1.wav")
 #define SE_ENEMY_DOWN_2 ("se_enemy_down_3.mp3")
 #define SE_ENEMY_DOWN_3 ("se_enemy_down.wav")
 
-/*
-//ボス
-#define SE_BOSS_DOWN_1 ("se_damage_1.mp3")
-#define SE_BOSS_APPEAR_1 ("voice_boss_appear_1.mp3")
-#define SE_BOSS_GAME_OVER_1 ("voice_boss_game_over_1.mp3")
-#define SE_BOSS_GAME_OVER_2 ("voice_boss_game_over_2.mp3")
-*/
-
-//ダメージ
-#define SE_DAMAGE_1 ("se_damage_1.mp3")
-
-//スワイプ
 //敵くっつくの音
 #define SE_SWIPE_1 ("se_swipe_1.mp3")
 #define SE_SWIPE_2 ("se_swipe_2.wav")
 //うがい時　音
 #define SE_SWIPE_3 ("se_swipe_3.mp3")
 
+//歯磨き粉ボム
 #define SE_POWDER_1 ("se_powder_1.mp3")
 #define SE_POWDER_2 ("se_powder_2.mp3")
 
+//未使用
 #define SE_FANFARE_1 ("se_fanfare_1.mp3")
-
 #define SE_GAME_OVER_1 ("se_game_over_1.mp3")
 
-#define SE_GET_ACHIEVEMENT_1 ("get_achievement_1.mp3")
-#define SE_GET_ACHIEVEMENT_2 ("get_achievement_2.mp3")
-#define SE_GET_ACHIEVEMENT_3 ("get_achievement_3.mp3")
-#define SE_GET_ACHIEVEMENT_4 ("get_achievement_4.mp3")
+
 
 #endif
